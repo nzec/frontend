32b36fd28544374076417d961d84bfe191f35836
Aditya Dutt <duttaditya18@gmail.com>
2018-10-25 00:08:15 +0530

    Wartex#4.0

---
+++ Wartex#4.0-2018-CSI-Web-Tech/README.md
@@ -0,0 +1,7 @@
+# Wartex#4.0-2018-CSI-Web-Tech
+Repository for CSI's submission for Web-Tech in Wartex#4.0 2018 (SRDAV, Dayanand Vihar)
+
+---
+
+## Results
+TBD
---
+++ Wartex#4.0-2018-CSI-Web-Tech/js/main.js
@@ -0,0 +1,33 @@
+function isOnScreen(elem) {
+	// if the element doesn't exist, abort
+	if(elem.length == 0) {
+		return;
+	}
+	var viewport_top = $(window).scrollTop();
+	var viewport_height = $(window).height();
+	var viewport_bottom = viewport_top + viewport_height;
+	var top = $(elem).offset().top;
+	var height = $(elem).height();
+	var bottom = top + height;
+
+	return (top >= viewport_top && top < viewport_bottom) ||
+	(bottom > viewport_top && bottom <= viewport_bottom) ||
+	(height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+}
+
+$(document).ready(function() {
+    function fadeInUp(elem) {
+        window.addEventListener('scroll', function(e) {
+            if(isOnScreen($(elem))) {
+                $(elem).addClass('fadeInUp');
+            } else {
+                $(elem).removeClass('fadeInUp');
+            }
+        });
+    }
+    fadeInUp('.sec-1 .left');
+    fadeInUp('.sec-1 .right');
+    fadeInUp('.sec-2 .left');
+    fadeInUp('.sec-2 .right');
+    fadeInUp('.sec-3 .inner-sec');
+});
